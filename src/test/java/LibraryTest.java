f80ab79d0e8aa6c6b16237dd56f82e1b89a96ced
Antonio Terreno <antonio.terreno@gmail.com>
2016-06-16 12:17:31 +0100

    Adding basic project structure and Idea project files to make it as simple as possible for candidates

---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class LibraryTest {
+    @Test
+    public void testSomeLibraryMethod() {
+        Library classUnderTest = new Library();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
